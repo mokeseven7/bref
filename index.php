55f732df1083e4a511bd755c9be1da85c072532b
Mike McGrath <mokeseven7@gmail.com>
2021-10-23 16:05:20 -0600

    api gateway

--- index.php
+++ index.php
@@ -10,6 +10,9 @@ class Handler extends SqsHandler
 {
     public function handleSqs(SqsEvent $event, Context $context): void
     {
+        var_dump($event);
+        var_dump($context);
+
         foreach ($event->getRecords() as $record) {
             // We can retrieve the message body of each record via `->getBody()`
             $body = $record->getBody();
